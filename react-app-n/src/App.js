63efaa77abe87eaaa870fb07d00322d9f2bd68a0
Nsuliman <nsuliman88@gmail.com>
2020-02-23 19:15:05 +0200

    react works

--- react-app-n/src/App.js
+++ react-app-n/src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// dummy component - it is also a functional component
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="words-app">
+      <h1>Words App - React Land</h1>
+    </header>
+  )
+}
+
+// also a dummy component and functional component
+const Footer = () => {
+  return (
+    <footer>my cool footer</footer>
+  )
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    // initial state
+    this.state = {
+      words: 'nothing to see here'
+    }
+  }
+
+  handleChange = e => {
+    let words = e.target.value;
+    this.setState({ words });
+    console.log('__STATE__', this.state);
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    let words = this.state.words.split('').reverse().join('');
+    this.setState({ words });
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>{this.state.words}</h3>
+        <input onChange={this.handleChange} />
+        <button onClick={this.handleClick}>click for words</button>
+      </section>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+        <Main />
+        <Main />
+        <Main />
+        <Footer />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
